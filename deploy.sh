7072112d16c482bbf25459fc1c33e9c8ca287a2e
Pranay Prakash <pranay.gp@gmail.com>
2018-09-19 20:39:13 -0500

    update mm2018 dep

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker build mm2018 -t pranaygp/mm
+docker push pranaygp/mm
 docker build . -t gcr.io/mechmania2017/runner:latest
 docker push gcr.io/mechmania2017/runner:latest
 kubectl apply -f app.yaml
