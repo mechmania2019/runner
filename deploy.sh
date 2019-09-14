5a1839bb90427daec51ac6d928877997db204d76
angiecheng <angie.cheng33@gmail.com>
2019-09-14 15:04:55 -0500

    changed made to game engine

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build mm2018 -t pranaygp/mm
+docker build mm25_game_engine -t pranaygp/mm
 docker push pranaygp/mm
 docker build . -t gcr.io/mechmania2017/runner:latest
 docker push gcr.io/mechmania2017/runner:latest
