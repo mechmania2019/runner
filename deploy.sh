1a30252ad5add2ace674c4269be96644d79115e2
njc1583 <njc1583@gmail.com>
2019-09-18 17:45:32 -0500

    Update deploy by removing building of the game engine

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker build mm25_game_engine -t pranaygp/mm
-docker push pranaygp/mm
 docker build . -t gcr.io/mechmania2017/runner:latest
 docker push gcr.io/mechmania2017/runner:latest
 kubectl apply -f app.yaml
